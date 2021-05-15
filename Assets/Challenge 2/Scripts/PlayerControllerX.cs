c673469916cd2b24aec673ea231e1cff622cf509
Sylvain CAU <ashdevfr@gmail.com>
2021-05-15 11:54:50 -0700

    Init

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private bool hasDog = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (!hasDog && Input.GetKeyDown(KeyCode.Space))
+        {
+            hasDog = true;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            Invoke("ResetHasDog", 1.0f);
+        }
+    }
+
+    void ResetHasDog()
+    {
+        hasDog = false;
+    }
+}
